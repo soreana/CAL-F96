34a046e67773a7ef2a86bef9250e2da6628a851c
sina kashipazha <esterlinkof@gmail.com>
2017-11-29 17:45:11 +0330

    Add Adder to IF stage.

--- IF_Stage.v
+++ IF_Stage.v
@@ -10,4 +10,6 @@ module IF_Stage
 
 	//assign Instruction = 32'b01110110010101000011001000010000; // check 7,6,5,4,3,2,1,0
 	assign Instruction = 32'b11111110110111001011101010011000; // check f,e,d,c,b,a,9,8
+	IF_Adder adder(.clk(clk),.current_pc(PC),.next_pc(PC) );
+
 endmodule
