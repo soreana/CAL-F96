5952ce807e2dba3bb038e81f47797999173a5958
sina kashipazha <esterlinkof@gmail.com>
2017-12-04 14:07:06 +0330

    Increase PC, take branch and reset PC_stage.

--- IF_Stage.v
+++ IF_Stage.v
@@ -4,12 +4,21 @@ module IF_Stage
 		input rst,
 		input Br_taken,
 		input [31:0] Br_offset,
-		output [31:0] PC,
+		output reg [31:0] PC,
 		output [31:0] Instruction
 	);
 
 	//assign Instruction = 32'b01110110010101000011001000010000; // check 7,6,5,4,3,2,1,0
 	assign Instruction = 32'b11111110110111001011101010011000; // check f,e,d,c,b,a,9,8
-	IF_Adder adder(.clk(clk),.current_pc(PC),.next_pc(PC) );
+
+  always @ (posedge clk or posedge rst)
+  if (rst) begin
+    	PC <= 32'd0;
+    end
+	else if (Br_taken) begin
+    	PC <= Br_offset;
+  end else begin
+    	PC <= PC + 32'd4;
+  end
 
 endmodule
