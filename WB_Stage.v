e6b7679917556e9bce07906af171caf10e112b3a
sina kashipazha <esterlinkof@gmail.com>
2017-12-27 09:44:58 +0330

    Complete everything except controll unit without testing.

--- WB_Stage.v
+++ WB_Stage.v
@@ -1,12 +1,9 @@
 module WB_stage
   (
     input clk,
-    input WB_EN_IN,
-    // MEM_Signals
 		input MEM_R_EN,
-    // memory Address
+    input WB_EN_IN,
     input [31:0] ALU_result,
-
     input [31:0] Mem_read_value,
     input [4:0] Dest_in,
 
@@ -14,5 +11,12 @@ module WB_stage
     output [31:0] write_value,
 		output [4:0] Dest
   );
+
+  assign write_value = (MEM_R_EN) ? Mem_read_value : ALU_result;
+
+  always @ (posedge clk) begin
+    WB_EN <= WB_EN_IN;
+    Dest <= Dest_in;
+  end
 
 endmodule
