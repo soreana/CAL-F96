aaf3e4e69fa2af18beda6c465927599b8a45c730
sina kashipazha <esterlinkof@gmail.com>
2018-01-08 01:51:43 +0330

    Fix Quartus Error.

--- IF_Stage_Registers.v
+++ IF_Stage_Registers.v
@@ -9,21 +9,18 @@ module IF_Stage_Registers
 		output reg [31:0] Instruction
 	);
 
-	always @ ( posedge clk or rst) begin
+	always @ (posedge clk or posedge rst) begin
 		if(rst) begin
 			PC <= 32'd0;
 			Instruction <= 32'd0;
 		end else begin
 			if(flush == 1'b1) begin
 				PC <= PC_in - 32'd1;
+				Instruction <= 32'd0;
 			end else begin
 				PC <= PC_in;
+				Instruction <= Instruction_in;
 			end
 		end
-			if(flush != 1'b1) begin
-				Instruction <= Instruction_in;
-			end else begin
-				Instruction <= 32'd0;
-			end
 	end
 endmodule
